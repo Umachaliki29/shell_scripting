73a2947b2792ac08ab951f512193e48ad899740b
Umachaliki29 <umashankari29@gmail.com>
2025-05-24 17:12:39 +0100

    commmitin clors

--- 11.functions.sh
+++ 11.functions.sh
@@ -1,52 +1,58 @@
-#!/bin/bash
+#!/bin/bash  # This indicates that the script should run using the bash shell
 
-USERID=$(id -u)  # Check root user or not
+# Get the user ID of the person running the script
+USERID=$(id -u)  # `id -u` returns 0 if root, otherwise a non-zero number
 
-if [ $USERID -ne 0 ]  # If not root user 
+# Check if the script is being run as root (user ID 0)
+if [ $USERID -ne 0 ]  # If the user ID is not 0, then the user is not root
 then
-    echo "ERROR:: Please run this script with root access"
-    exit 1 #give other than 0 upto 127
+    echo "ERROR:: Please run this script with root access"  # Print error message
+    exit 1  # Exit the script with a non-zero status to indicate failure
 else
-    echo "You are running with root access"
+    echo "You are running with root access"  # Inform the user that they have root privileges
 fi
 
-# validate functions takes input as exit status, what command they tried to install
-VALIDATE(){  
-    if [ $1 -eq 0 ]   # providing arguments to function as $1
+# Define a function to validate the success or failure of a command
+# Takes two parameters: $1 = exit status of the command, $2 = name of the package
+VALIDATE(){
+    if [ $1 -eq 0 ]  # If the exit status is 0, the command succeeded
     then
-        echo "Installing $2 is ... SUCCESS"
+        echo "Installing $2 is ... SUCCESS"  # Print success message for the package
     else
-        echo "Installing $2 is ... FAILURE"
-        exit 1
+        echo "Installing $2 is ... FAILURE"  # Print failure message for the package
+        exit 1  # Exit the script due to failure
     fi
 }
 
-dnf list installed mysql
-if [ $? -ne 0 ]
+# Check if MySQL is already installed using dnf
+dnf list installed mysql  # List installed packages and check for 'mysql'
+if [ $? -ne 0 ]  # If the exit status is not 0, MySQL is not installed
 then
-    echo "MySQL is not installed... going to install it"
-    dnf install mysql -y
-    VALIDATE $? "MySQL"
+    echo "MySQL is not installed... going to install it"  # Inform user
+    dnf install mysql -y  # Install MySQL without prompting for confirmation
+    VALIDATE $? "MySQL"  # Call the validation function with the status and package name
 else
-    echo "MySQL is already installed...Nothing to do"
+    echo "MySQL is already installed...Nothing to do"  # Inform user
 fi
 
-dnf list installed python3
-if [ $? -ne 0 ]
+# Check if python3 is already installed
+dnf list installed python3  # Check for python3 installation
+if [ $? -ne 0 ]  # If not installed
 then
-    echo "python3 is not installed... going to install it"
-    dnf install python3 -y
-    VALIDATE $? "python3"
+    echo "python3 is not installed... going to install it"  # Inform user
+    dnf install python3 -y  # Install python3
+    VALIDATE $? "python3"  # Validate the result of the installation
 else
-    echo "python3 is already installed...Nothing to do"
+    echo "python3 is already installed...Nothing to do"  # Inform user
 fi
 
-dnf list installed nginx
-if [ $? -ne 0 ]
+# Check if nginx is already installed
+dnf list installed nginx  # Check for nginx installation
+if [ $? -ne 0 ]  # If nginx is not installed
 then
-    echo "nginx is not installed... going to install it"
-    dnf install nginx -y
-    VALIDATE $? "nginx"
+    echo "nginx is not installed... going to install it"  # Inform user
+    dnf install nginx -y  # Install nginx
+    VALIDATE $? "nginx"  # Validate installation
 else
-    echo "nginx is already installed...Nothing to do"
+    echo "nginx is already installed...Nothing to do"  # Inform user
 fi
