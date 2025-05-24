821cc17f3259d38bcbc7b592e48b699e91112133
Umachaliki29 <umashankari29@gmail.com>
2025-05-24 15:41:09 +0100

    commiting

--- 10.installmysql.sh
+++ 10.installmysql.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 USERID=$(id -u)  # Check with root access 
-if[ $USERID -ne 0] # If root user proceed to install
+if[ $USERID -ne 0 ] # If root user proceed to install
 then 
     echo "ERROR: please run with root access"  # else show the error as please run with root access
+    exit 1 # give other than 0 upto 127
 else 
     echo "you are running with root access"
 fi
